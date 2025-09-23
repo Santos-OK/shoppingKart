27063a1c9f737b974bbb9e3a709147adb47d5ba6
Santos-OK <0262142@up.edu.mx>
2025-09-23 13:02:58 -0600

    Add files via upload

---
+++ shoppingKart/src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar"
+import Catalog from "./components/Catalog"
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <Catalog/>
+    </>
+  )
+}
+
+export default App
---
+++ shoppingKart/src/components/Catalog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Catalog() {
+  return (
+    <div className='flex flex-wrap justify-center'>
+        <div className='shadow shadow-blue-900 border rounded-2xl w-4/9'>
+            <p className='flex justify-center'>Ballon D'or</p>
+        </div>
+    </div>
+  )
+}
